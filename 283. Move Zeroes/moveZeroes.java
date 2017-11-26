39e8a337f21f495c93749b91f95a9274fb0cadf5
OrcasChen <598544819@qq.com>
2017-11-26 23:01:54 +0800

    moveZeroes
    把.txt格式转换成.java

--- 283. Move Zeroes/moveZeroes.java
+++ 283. Move Zeroes/moveZeroes.java
@@ -1,3 +1,8 @@
+//Given an array nums, write a function to move all 0's to the end of it 
+//while maintaining the relative order of the non-zero elements.
+//For example, given nums = [0, 1, 0, 3, 12], after calling your function, 
+//nums should be [1, 3, 12, 0, 0].
+//遍历数组，用异或交换非零与0
 class Solution {
 public void moveZeroes(int[] nums) { 
  int index = 0;
